bb9d8d11b1b246880b8a2240eb995da91240757a
Bibil Mathew Chacko <bibil.mathew@mindlabssolutions.com>
2020-07-10 01:53:40 +0530

    On Development

--- src/main/java/backendems/entities/Expense.java
+++ src/main/java/backendems/entities/Expense.java
@@ -2,8 +2,11 @@ package backendems.entities;
 
 import java.time.LocalDateTime;
 
+import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -19,13 +22,14 @@ public class Expense
 {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
     @NotBlank
     private String particulars;
     
-    @NotBlank
+    
     @NotBlank
     private String comments;
 
