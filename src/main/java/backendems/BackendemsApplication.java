871e9558562dba7425e564e5f9746202dc95d28d
Bibil Mathew Chacko <bibil.mathew@mindlabssolutions.com>
2020-07-09 23:03:47 +0530

    first commit

---
+++ src/main/java/backendems/BackendemsApplication.java
@@ -0,0 +1,13 @@
+package backendems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backendems/BackendemsApplicationTests.java
@@ -0,0 +1,13 @@
+package backendems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
